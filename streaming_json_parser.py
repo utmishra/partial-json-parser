eae67ed913158a3da9c31cb3c5f1b9a2dd7ffc65
Utkarsh Mishra <utmishra@gmail.com>
2025-02-24 21:54:32 +0100

    feat: Handle partial json

--- streaming_json_parser.py
+++ streaming_json_parser.py
@@ -8,6 +8,7 @@ class ParsingState(Enum):
     EXPECTING_VALUE = 2
     EXPECTING_COLON = 3
     EXPECTING_PARTIAL_VALUE = 4
+    EXPECTING_PARTIAL_KEY = 5
 
 
 class StreamingJsonParser:
@@ -15,7 +16,8 @@ class StreamingJsonParser:
         self.root: dict = {}
         self.stack: List[str] = []
         self.last_key: str | None = None
-        self.partial_token = ""
+        self.partial_token_value = ""
+        self.partial_token_key = ""
         self.current_state: ParsingState = ParsingState.START
 
     def consume(self, buffer: str):
@@ -24,20 +26,29 @@ class StreamingJsonParser:
         while pos < len(buffer):
             char = buffer[pos]
 
+            if self.current_state in [
+                ParsingState.EXPECTING_KEY,
+                ParsingState.EXPECTING_PARTIAL_KEY,
+            ] and char in [","]:
+                raise ValueError(
+                    "Invalid symbol. Expecting a valid/partial key or value"
+                )
+
+            if self.current_state == ParsingState.EXPECTING_PARTIAL_KEY:
+                self.handle_partial_token_key(char)
+                pos += 1
+                continue
+
             if self.current_state == ParsingState.EXPECTING_PARTIAL_VALUE:
-                if char == '"':
-                    self.handle_completed_token()
-                else:
-                    self.partial_token += char
-                    self.handle_partial_token()
+                self.handle_partial_token_value(char)
                 pos += 1
                 continue
 
             match char:
-                case " " | ",":
+                case " " | "," | "}":
                     pos += 1
                 case "{":
-                    self.handle_start_quote()
+                    self.handle_new_object()
                     pos += 1
                 case ":":
                     if self.current_state == ParsingState.EXPECTING_COLON:
@@ -45,17 +56,10 @@ class StreamingJsonParser:
                     pos += 1
                 case '"':
                     pos = self.parse_quotes(buffer, pos)
-                case "}":
-                    if self.current_state == ParsingState.EXPECTING_PARTIAL_VALUE:
-                        raise ValueError(
-                            f"Expecting {self.last_key}'s value. (Ending) curly brace passed"
-                        )
-
-                    pos += 1
                 case "[" | "]":
                     raise ValueError("Array's are currently not supported")
 
-    def handle_start_quote(self):
+    def handle_new_object(self):
         if self.current_state == ParsingState.EXPECTING_PARTIAL_VALUE:
             raise ValueError(
                 f"Expecting {self.last_key}'s value. (Starting) curly brace passed"
@@ -75,8 +79,13 @@ class StreamingJsonParser:
         end_quote_pos = buffer.find('"', pos + 1)
 
         if end_quote_pos < 0:
-            self.partial_token = buffer[pos + 1 :]
-            self.handle_partial_token()
+            partial_token = buffer[pos + 1 :]
+            if not self.last_key:
+                self.current_state = ParsingState.EXPECTING_PARTIAL_KEY
+                self.handle_partial_token_key(partial_token)
+            else:
+                self.current_state = ParsingState.EXPECTING_PARTIAL_VALUE
+                self.handle_partial_token_value(partial_token)
 
             return len(buffer)
 
@@ -92,16 +101,30 @@ class StreamingJsonParser:
 
         return end_quote_pos + 1
 
-    def handle_partial_token(self) -> int:
+    def handle_partial_token_key(self, char: str):
+        if char == '"':
+            self.handle_completed_token_key()
+        else:
+            self.partial_token_key += char
+
+    def handle_completed_token_key(self):
+        self.last_key = self.partial_token_key
+        self.partial_token_key = ""
+        self.current_state = ParsingState.EXPECTING_VALUE
+
+    def handle_partial_token_value(self, char: str) -> int:
+        if char == '"':
+            return self.handle_completed_token_value()
+        else:
+            self.partial_token_value += char
+
         if not self.last_key:
             raise BufferError("Invalid symbol found, expecting value")
 
-        self.current_state = ParsingState.EXPECTING_PARTIAL_VALUE
+        self.stack[self.last_key] = self.partial_token_value
 
-        self.stack[self.last_key] = self.partial_token
-
-    def handle_completed_token(self):
-        self.partial_token = None
+    def handle_completed_token_value(self):
+        self.partial_token_value = ""
         self.last_key = None
         self.current_state = ParsingState.EXPECTING_KEY
 
